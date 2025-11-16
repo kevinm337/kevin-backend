750f09a7f422ec10ef3e142d9877929f6aa0ab8b
Kevin Mathew <kevinmathew@Kevins-MacBook-Air.local>
2025-11-16 05:50:06 -0600

    Fix db import path

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');  // Correct db import
+const pool = require('../models/db');  // Correct db import
 
 const {
   getAllPosts,
